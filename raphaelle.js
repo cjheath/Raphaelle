99d6511888e92041299a8768d803a4f7df6bbfd1
Clifford Heath <clifford.heath@gmail.com>
2009-11-27 16:29:20 +1100

    Allow a click on one thing to drag another & other fixes

--- raphaelle.js
+++ raphaelle.js
@@ -1,43 +1,51 @@
-Raphael.el.draggable = function() {
-  var raph_obj = this;
-  if (typeof this.node === 'undefined' && typeof this.paper === 'undefined')
-  {
-    log(this + 'is not a raphj obj so you can\'t make it draggabe');
+// A click on this Raphael element causes "to_drag" to start dragging
+Raphael.el.draggable = function(to_drag) {
+  var drag_obj = to_drag != null ? to_drag : this;
+
+  // Check that this is an ok thing to even think about:
+  if (typeof this.node === 'undefined' && typeof this.paper === 'undefined') {
+    console.log(this+' is not a Raphael object so you can\'t make it draggable');
     return;
   }
-  
+  if (typeof drag_obj.node === 'undefined' && typeof drag_obj.paper === 'undefined') {
+    console.log(drag_obj+' is not a Raphael object so you can\'t make it draggable');
+    return;
+  }
+
   $(this.node).mousedown(mousedown);
-  
+
   function mousedown(event) {
-    // log(raph_obj.node);
-    // var node = event.target;
+    // console.log("Over "+event.target);
 
     var node = this;
-    
+    var started = false;
+    var last_x, last_y;
+    last_x = event.clientX;
+    last_y = event.clientY;
+
     var mousemove = function(event) {
-      //Hack to prevent firefox from sometimes dragging the canvas element
-      if(event.preventDefault) {
+      // Hack to prevent Firefox from sometimes dragging the canvas element
+      if (event.preventDefault) {
         event.preventDefault();
       }
 
-      //Set the xy coords 
-      if (raph_obj.type == 'circle') {
-        //circles use cx/cy for center of object instead of x/y, per the SVG spec
-        raph_obj.attr('cx', event.clientX);
-        raph_obj.attr('cy', event.clientY);
+      var delta_x = last_x-event.clientX;
+      var delta_y = last_y-event.clientY;
+      if (!started && (delta_x>3 || delta_x<-3 || delta_y>3 || delta_y<-3)) {
+	started = true;
       }
-      else
-      {
-        raph_obj.attr('x', event.clientX);
-        raph_obj.attr('y', event.clientY);
-      }
+      if (!started) return;
+
+      drag_obj.translate(event.clientX-last_x, event.clientY-last_y)
+      last_x = event.clientX;
+      last_y = event.clientY;
     }
 
     var mouseup = function(event) {
       $(node.raphael.paper.canvas).unbind('mousemove', mousemove);
-      $(window).unbind('mouseup', mouseup);     
+      $(window).unbind('mouseup', mouseup);
     }
     $(node.raphael.paper.canvas).mousemove(mousemove);
-    $(window).mouseup(mouseup);   
-  } 
+    $(window).mouseup(mouseup);
+  }
 }
