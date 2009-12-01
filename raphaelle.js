8df715ee4bf96e8b467e49b0c40ba46cff72e8ef
Clifford Heath <clifford.heath@gmail.com>
2009-12-01 16:16:41 +1100

    Dragging one of many objects now works, as does cancellation, and drag_over and dropped_on objects are added

--- raphaelle.js
+++ raphaelle.js
@@ -1,27 +1,30 @@
 // A click on this Raphael element causes "to_drag" to start dragging.
 // right_button = null means drag using either, otherwise its false or true for left or right.
-// If the object being dragged has an updateDrag(x, y, ctrl, shift, alt), the default one isn't added
-// If the object has startDrag(x, y) , and finishDrag(x, y), those are called.
+// If the object being dragged has an updateDrag(dragging_over, x, y, ctrl, shift, alt), the default one isn't added
+// If the object has startDrag(x, y) , and finishDrag(dropped_on, x, y), those are called.
 Raphael.el.draggable = function(to_drag, right_button) {
-  var drag_obj = to_drag != null ? to_drag : this;
+  var drag_obj;
+  // drag_obj = to_drag != null ? to_drag : this;
+  if (to_drag != null) { drag_obj = to_drag; } else { drag_obj = this; }
 
   // Check that this is an ok thing to even think about:
-  if (typeof this.node === 'undefined' && typeof this.paper === 'undefined') {
+  if (typeof this.node == 'undefined' && typeof this.paper == 'undefined') {
     console.log(this+' is not a Raphael object so you can\'t make it draggable');
     return;
   }
-  if (typeof drag_obj.node === 'undefined' && typeof drag_obj.paper === 'undefined') {
+  if (typeof drag_obj.node == 'undefined' && typeof drag_obj.paper == 'undefined') {
     console.log(drag_obj+' is not a Raphael object so you can\'t make it draggable');
     return;
   }
 
   $(this.node).mousedown(
     function (event) {
-      // var target = event.target == null ? event.target : event.srcElement;  // Firefox/IE
-      // if (target.nodeType == 3) target = target.parentNode;	// Safari
-      // console.log("Over "+target);
+      var dragging = true;
+      var target = typeof event.target != 'undefined' ? event.target : event.srcElement;  // Firefox/IE
+      if (target.nodeType == 3) target = target.parentNode;	// Safari
+      // console.log("MouseDown on "+drag_obj.node.id+" with target="+target.id);
 
-      if (right_button != null && (right_button == true) != (event.button > 1)) return;
+      if (right_button !== null && (right_button === true) !== (event.button > 1)) { return; }
 
       var node = this;
       var started = false;
@@ -30,7 +33,12 @@ Raphael.el.draggable = function(to_drag, right_button) {
       start_x = last_x = event.clientX;
       start_y = last_y = event.clientY;
 
-      // REVISIT: Bring the object to the front so it doesn't drag behind things, and restore it later
+      var over = function(event) {
+	drag_obj.hide();
+	var dragging_over = document.elementFromPoint(event.clientX, event.clientY);
+	drag_obj.show();
+	return dragging_over;
+      }
 
       var mousemove = function(event) {
 	// Hack to prevent Firefox from sometimes dragging the canvas element
@@ -44,76 +52,64 @@ Raphael.el.draggable = function(to_drag, right_button) {
 	if (!started && (delta_x>3 || delta_x<-3 || delta_y>3 || delta_y<-3)) {
 	  started = true;
 
-	  if (drag_obj.updateDrag == null) {
-	    drag_obj.updateDrag = function(xdelta, ydelta) {
-	      drag_obj.translate(xdelta, ydelta);
-	    }
-	  }
-	  if (drag_obj.startDrag != null) {
-	    drag_obj.startDrag(last_x, last_y)
+	  // REVISIT: Bring the object to the front so it doesn't drag behind things, and restore it later
+	  if (typeof drag_obj.startDrag != 'undefined') {
+	    drag_obj.startDrag(last_x, last_y);
 	  }
 	}
-	// console.log("X="+event.clientX+", Y="+event.clientY);
-	if (!started) return;
+	if (!started) { return; }
 
-	/*
-	// Start hit test
-	var rect = paper.canvas.createSVGRect();
-	rect.x = event.clientX-2;
-	rect.y = event.clientY-2;
-	rect.width = 5;
-	rect.height = 5;
-	// console.log(rect);
-	// Find all objects that intersect this rect which are below the drag_obj
-	var hits = paper.canvas.getIntersectionList(rect, null); //, drag_obj.node);
-	for (hit in hits) console.log(hit);
-	// end hit test
-	*/
-
-	drag_obj.updateDrag(event.clientX-last_x, event.clientY-last_y, event.ctrlKey, event.shiftKey, event.altKey);
+	var dragging_over = over(event);
+	// console.log("Move "+drag_obj.node.id+" over "+dragging_over.id+" to X="+event.clientX+", Y="+event.clientY);
+	drag_obj.updateDrag(dragging_over, event.clientX-last_x, event.clientY-last_y, event.ctrlKey, event.shiftKey, event.altKey);
 	last_x = event.clientX;
 	last_y = event.clientY;
-      }
+      };
+
+      var revert;
+      var cancel;
 
       // Process keyboard input so we can cancel
       var key = function(event) {
 	var code;
-	if (!event) event = window.event;
-	if (event.keyCode) code = event.keyCode;
-	else if (event.which) code = event.which;
+	if (!dragging) { return; }  // The unbind on keypress should do this for us anyhow.
+	if (!event) { event = window.event; }
+	if (event.keyCode) { code = event.keyCode; }
+	else if (event.which) { code = event.which; }
 	var character = String.fromCharCode(code);
 	// console.log("key="+code+" char="+character);
 	if (code == 27) { // Escape
 	  revert(event);
 	  cancel();
 	}
-      }
+      };
 
       // Revert to starting location
-      var revert = function(event) {
-	if (!started) return;
+      revert = function(event) {
+	if (!started) { return; }
 	// console.log("start_x="+start_x+", start_y="+start_y+"; last_x="+last_x+", last_y="+last_y);
-	drag_obj.updateDrag(start_x-last_x, start_y-last_y, event.ctrlKey, event.shiftKey, event.altKey);
+	drag_obj.updateDrag(null, start_x-last_x, start_y-last_y, event.ctrlKey, event.shiftKey, event.altKey);
 	started = false;  // Sometimes get the same event twice.
-      }
-
-      // Undo the setup for a drag
-      var cancel = function() {
-	$(node.raphael.paper.canvas).unbind('mousemove', mousemove);
-	$(node.raphael.paper.canvas).unbind('key', key);
-	$(window).unbind('mouseup', mouseup);
-      }
+      };
 
       // The drag has ended, deal with it.
       var mouseup = function(event) {
 	if (started) {
-	  // console.log("You dropped "+drag_obj+" and almost broke it");
-	  if (drag_obj.finishDrag != null) {
-	    drag_obj.finishDrag(last_x, last_y)
+	  var dropped_on = over(event);
+	  if (typeof drag_obj.finishDrag != 'undefined') {
+	    drag_obj.finishDrag(dropped_on, last_x, last_y);
 	  }
 	}
 	cancel();
-      }
+      };
+
+      // Undo the setup for a drag
+      cancel = function() {
+	$(node.raphael.paper.canvas).unbind('mousemove', mousemove);
+	$(node.raphael.paper.canvas).unbind('keypress', key);
+	$(window).unbind('mouseup', mouseup);
+	dragging = false;
+      };
 
       // Bind the appropriate events:
       $(window).keypress(key);
@@ -121,4 +117,4 @@ Raphael.el.draggable = function(to_drag, right_button) {
       $(window).mouseup(mouseup);
     }
   );
-}
+};
